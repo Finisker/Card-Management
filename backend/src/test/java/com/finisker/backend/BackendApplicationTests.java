19d57297a47f03f97fb5be67cd35bd835863fb77
Maciej Plachta <macpla08@gmail.com>
2025-06-10 20:20:02 +0200

    first commit

---
+++ backend/src/test/java/com/finisker/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finisker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
