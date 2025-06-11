8c9415a9622bbfb14cd560c21616ce960533d8c3
Maciej Plachta <macpla08@gmail.com>
2025-06-11 13:17:09 +0200

    Basic data model classes

--- backend/src/main/java/com/finisker/backend/persistence/entity/Card.java
+++ backend/src/main/java/com/finisker/backend/persistence/entity/Card.java
@@ -6,6 +6,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -20,9 +23,13 @@ public class Card {
 
     private String title;
     private String type;
+    private String discription;
+    private String image_path;
+
     private Integer mana_cost;
     private Integer gold_cost;
+
+    @ManyToMany
+    @JoinTable(name = "card_tag", joinColumns = @JoinColumn(name = "card_id"), inverseJoinColumns = @JoinColumn(name = "tag_id"))
     private Set<Tag> tags;
-    private String discription;
-    private String imagePath;
 }
