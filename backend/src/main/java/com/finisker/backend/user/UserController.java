8195af2dedc43163eeb62bc1a57678c869e9e7d0
Maciej Plachta <macpla08@gmail.com>
2025-06-11 11:24:20 +0200

    Delete mapping tests

--- backend/src/main/java/com/finisker/backend/user/UserController.java
+++ backend/src/main/java/com/finisker/backend/user/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,5 +30,10 @@ public class UserController {
     @PostMapping("/add")
     public void createUser(@RequestBody User user) {
         userService.addUser(user);
+    }
+
+    @DeleteMapping("/deleteAll")
+    public void deleteUsers() {
+        userService.deleteAll();
     }
 }
--- backend/src/main/java/com/finisker/backend/user/UserService.java
+++ backend/src/main/java/com/finisker/backend/user/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
         userRepository.save(user);
     }
 
+    public void deleteAll() {
+        userRepository.deleteAll();
+    }
+
 }
