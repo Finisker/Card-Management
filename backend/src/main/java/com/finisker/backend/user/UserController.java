913ec5928d2eef4681a2b5870810ce4aa2513290
Maciej Plachta <macpla08@gmail.com>
2025-06-11 11:32:25 +0200

    fixed mapping

--- backend/src/main/java/com/finisker/backend/user/UserController.java
+++ backend/src/main/java/com/finisker/backend/user/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "")
+@RequestMapping(path = "users")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
     private final UserService userService;
