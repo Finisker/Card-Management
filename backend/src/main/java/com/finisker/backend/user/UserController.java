2e6ac0bdb6b1fbea9dab945ceebe3b8b11725db3
Maciej Plachta <macpla08@gmail.com>
2025-06-11 11:19:37 +0200

    PostMapping for testing

--- backend/src/main/java/com/finisker/backend/user/UserController.java
+++ backend/src/main/java/com/finisker/backend/user/UserController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "api/v1/users")
+@RequestMapping(path = "")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
     private final UserService userService;
@@ -22,5 +24,10 @@ public class UserController {
     @GetMapping("/all")
     public List<User> getUsers() {
         return userService.getUsers();
+    }
+
+    @PostMapping("/add")
+    public void createUser(@RequestBody User user) {
+        userService.addUser(user);
     }
 }
--- backend/src/main/java/com/finisker/backend/user/UserService.java
+++ backend/src/main/java/com/finisker/backend/user/UserService.java
@@ -19,4 +19,8 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
+
 }
