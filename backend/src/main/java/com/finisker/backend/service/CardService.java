b9682fcf795c1f832ec42b7d5e3a8ab6b386f721
Maciej Plachta <macpla08@gmail.com>
2025-06-12 15:56:42 +0200

    HTTP methods cleanup

--- backend/src/main/java/com/finisker/backend/service/CardService.java
+++ backend/src/main/java/com/finisker/backend/service/CardService.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Service;
 import com.finisker.backend.model.dto.CardDTO;
 import com.finisker.backend.model.mapper.CardMapper;
 import com.finisker.backend.persistence.entity.Card;
+import com.finisker.backend.persistence.entity.Tag;
 import com.finisker.backend.persistence.respository.CardRepository;
+import com.finisker.backend.persistence.respository.TagRepository;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -16,11 +19,13 @@ public class CardService {
 
     private final CardRepository cardRepository;
     private final CardMapper cardMapper;
+    private final TagRepository tagRepository;
 
     @Autowired
-    public CardService(CardRepository cardRepository, CardMapper cardMapper) {
+    public CardService(CardRepository cardRepository, CardMapper cardMapper, TagRepository tagRepository) {
         this.cardRepository = cardRepository;
         this.cardMapper = cardMapper;
+        this.tagRepository = tagRepository;
     }
 
     public List<CardDTO> getAllCards() {
@@ -29,8 +34,17 @@ public class CardService {
         return dtos;
     }
 
-    public void addCard(Card card) {
-        cardRepository.save(card);
+    public Card createCard(CardDTO dto) {
+        Card card = cardMapper.toEntity(dto);
+
+        Set<Tag> existingTags = dto.getTags().stream()
+                .map(tagDTO -> tagRepository.findById(tagDTO.getId())
+                        .orElseThrow(() -> new RuntimeException("Tag not found: " + tagDTO.getId())))
+                .collect(Collectors.toSet());
+
+        card.setTags(existingTags);
+
+        return cardRepository.save(card);
     }
 
     public void deleteAll() {
