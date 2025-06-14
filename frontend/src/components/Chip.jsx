ebc5ec8bf299502f94e079479fdde79cd947c62a
Maciej Plachta <macpla08@gmail.com>
2025-06-14 17:40:45 +0200

    Search by tags indicators

--- frontend/src/components/Chip.jsx
+++ frontend/src/components/Chip.jsx
@@ -1,11 +1,15 @@
-import CloseButton from "react-bootstrap/CloseButton";
+import { Children } from "react";
 import "../styles/Chip.css";
 
 export default function Chip(props) {
+  const children = Children.toArray(props.children);
+
   return (
-    <div className="container">
-      <span>{props.children}</span>
-      <CloseButton />
+    <div
+      className="container chip-container mb-3"
+      onClick={props.handleOnClick}
+    >
+      <span>{children}</span>
     </div>
   );
 }
